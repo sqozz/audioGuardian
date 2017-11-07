3784a9e577a5874cebf9ef131fb93c4daae5c5f1
Sqozz <sqozz@geekify.de>
2017-11-07 16:59:19 +0100

    Fix stability and expect more connection errors

--- mpd_watchdog.py
+++ mpd_watchdog.py
@@ -11,6 +11,15 @@ io = os.open("/tmp/mpd_watchdog", os.O_RDONLY | os.O_NONBLOCK)
 last_recv = time.time()
 mpd_client = MPDClient()
 
+def mpd_connect():
+	try:
+		mpd_client.connect("localhost", 6600)
+	except ConnectionError as err:
+		pass
+	except Exception:
+		pdb.set_trace() # most likely something i did not think about died (e.g. timeout foo)
+
+
 while True:
 	try:
 		buffer = os.read(io, 1024)
@@ -24,20 +33,21 @@ while True:
 			raise  # something else has happened -- better reraise
 
 	if not buffer_filled and last_recv + 2 <= time.time(): # give the socket 2 seconds time before checking the mpd status
-		try:
-			mpd_client.connect("localhost", 6600)
-		except ConnectionError as err:
-			pass
-		except Exception:
-			pdb.set_trace() # most likely something i did not think about died (e.g. timeout foo)
-			# TODO: back off (reduce retries) until mpd is available again
+		print("buffer only nulls")
+		mpd_connect()
+		# TODO: back off (reduce retries) until mpd is available again
 
 		status = mpd_client.status()
 		if status["state"] == "play":
 			# mpd is playing but no data flowing? Quick, recover!
 			print("stall detected, resetting")
-			mpd_client.stop()
-			mpd_client.play()
+			try:
+				mpd_client.stop()
+				mpd_client.play()
+			except BrokenPipeError:
+				mpd_connect()
+			else:
+				last_recv = time.time()
 			# TODO: make this smarter and escalate after some tries and restart mpd
 			# call(["/etc/init.d/mpd", "restart"]) # a really hard way
 
